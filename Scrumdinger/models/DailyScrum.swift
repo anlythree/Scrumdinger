b19612bba388050304ac8d72c482a053c2aa90e8
anlythree <15610462126@163.com>
2023-09-17 14:10:53 +0800

    first commit

---
+++ Scrumdinger/models/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by anlythree on 2023/9/17.
+//
+
+import Foundation
---
+++ Scrumdinger/models/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by anlythree on 2023/9/17.
+//
+
+import Foundation
